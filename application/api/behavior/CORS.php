6a7f2b984ea57879310e22dfe997b67de54fe1c4
w2xi <43wangxi@gmail.com>
2021-03-23 23:52:26 +0800

    resolve merge conflict

--- application/api/behavior/CORS.php
+++ application/api/behavior/CORS.php
@@ -6,7 +6,7 @@ class CORS
 {
     public function appInit(&$params)
     {
-        header('Access-Control-Allow-Origin: http://10.40.2.199:8080');
+        header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept, Authorization");
         header('Access-Control-Allow-Methods: POST,GET,OPTION');
 
