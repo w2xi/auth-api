e37b2398436dc531d754887cafa46cfa4321413f
w2xi <43wangxi@gmail.com>
2021-04-05 21:08:55 +0800

    upload file with tp-file-conponent

--- application/api/controller/v1/UploadFile.php
+++ application/api/controller/v1/UploadFile.php
@@ -2,13 +2,14 @@
 
 namespace app\api\controller\v1;
 
-
+use think\Request;
+use think\File;
 
 class UploadFile 
 {
-    public function index()
+    public function test()
     {
-        return UPLOAD_PATH;
+        echo md5_file(UPLOAD_PATH . 'test/07/ea335b77ad23bb411cdcdbc82f7c55.png');
     }
 
     public function upload()
@@ -33,14 +34,55 @@ class UploadFile
         return json($data);
     }
 
+    public function uploadWithTp(Request $request)
+    {
+        // $file = $_FILES['file'];
+        // echo '<pre>';
+        // print_r($file);
+
+        // multiple file upload
+
+        $file = $request->file('file');
+
+        foreach ($file as $value) {
+            $this->singleUpload($value);
+        }
+
+        // $this->singleUpload($file);
+    }
+
+    public function singleUpload(File $file = null)
+    {
+        $file = $file ?? request()->file('file');
+
+        if ( $file ){
+            $info = $file->validate(['size'=>1024*1024*3, 'ext'=>'png,jpg,jpeg'])->rule('md5')->move(UPLOAD_PATH . 'test/');
+            if ( $info ){
+                echo 'extension: ' . $info->getExtension() . '<br />';
+                echo 'filename: ' . $info->getFilename() . '<br />';
+                echo 'saveName: ' . $info->getSaveName() . '<br />';
+                echo $info->md5() . '<br />';
+            }else{
+                return _error($file->getError());
+            }
+        }else{
+            return _error('请选择要是上传的文件');
+        }
+    }
+
     public function submit()
     {
-        echo '<form enctype="multipart/form-data" action="upload" method="POST">
-                <!-- MAX_FILE_SIZE must precede the file input field -->
-                <input type="hidden" name="MAX_FILE_SIZE" value="1073741824" />
-                <!-- Name of input element determines name in $_FILES array -->
-                Send this file: <input name="file" type="file" />
-                <input type="submit" value="Send File" />
-            </form>';
+        // nowdoc
+        $form = <<<'EOD'
+<form enctype="multipart/form-data" action="uploadWithTp" method="POST">
+<!-- MAX_FILE_SIZE must precede the file input field -->
+<input type="hidden" name="MAX_FILE_SIZE" value="1073741824" />
+<!-- Name of input element determines name in $_FILES array -->
+Send this file: 
+<input name="file[]" type="file" multiple />
+<input type="submit" value="Send File" />
+</form>
+EOD;
+        echo $form;
     }
 }
