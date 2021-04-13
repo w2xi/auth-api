74a0e2f137a39d867c069a66fb6fd1ed36b270c4
w2xi <43wangxi@gmail.com>
2021-04-13 23:48:53 +0800

    reading tp5 source code

---
+++ application/api/behavior/Test.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace app\api\behavior;
+
+class Test
+{
+    public function run(&$params)
+    {
+        // dd('run');
+    }
+
+    public static function helloTest()
+    {
+        // dd('helloTest');
+    }
+
+    public function arrayTest()
+    {
+        // dd('arrayTest');
+    }
+}
